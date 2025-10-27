2659dda70c33080ac33e8659073f9c3bb0778f12
Raghuvansh Agarwal <rarchva@gmail.com>
2025-10-27 17:24:03 +0530

    grid setup

---
+++ Assets/Scripts/GridPosition.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class GridPosition : MonoBehaviour
+{
+    [SerializeField] private int gridX;
+    [SerializeField] private int gridY;
+    private void OnMouseDown() {
+        Debug.Log($"{gridY}, {gridX}");
+    }
+}
